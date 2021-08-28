75e41be3753bed4b2c9d4ae427aae5bc118f5719
CHAA-C <zeronert05@gmail.com>
2021-08-28 13:37:41 +0700

    first commit

--- BIGGEST_SUM-1.cpp
+++ BIGGEST_SUM-1.cpp
@@ -11,13 +11,13 @@ int main(){
 	int sum3=n2+n3;
 	
 	if (sum1 > sum2 && sum1 > sum3){
-		printf("\nThe biggest sum nation is %d",sum1);
+		printf("\nThe biggest sum nation is %d + %d = %d",n1,n2,sum1);
 	}
 	else if (sum2 > sum1 && sum2 > sum3){
-		printf("\nThe biggest sum nation is %d",sum2);
+		printf("\nThe biggest sum nation is %d + %d = %d",n1,n3,sum2);
 	}
 	else if (sum3 > sum1 && sum3 > sum2){
-		printf("\nThe biggest sum nation is %d",sum3);
+		printf("\nThe biggest sum nation is %d + %d = %d",n2,n3,sum3);
 	}
 	
 		return 0;
